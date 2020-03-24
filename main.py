afc565bd6afda7a0f0cdb0d42f1741c8692a03dd
aziraphael <aziriphael@protonmail.com>
2020-03-24 13:46:42 -0700

    added movement

--- main.py
+++ main.py
@@ -18,8 +18,17 @@ robot = pygame.image.load('sprite.png')
 robo_rect = robot.get_rect()
 robo_rect.center = (width//2,height//2)
 
+#robot speed and stuff
+speed = pygame.math.Vector2(0,10)
+
+
+
+
+
 #actual code stufffffffff
 
+def moveRobot():
+    robo_rect.move_ip(speed)
 
 
 def main():
@@ -32,10 +41,11 @@ def main():
     
     while True:
         clock.tick(60)
-        
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
+        moveRobot()
         screen.fill(color)
         screen.blit(robot,robo_rect)
         pygame.display.flip()
